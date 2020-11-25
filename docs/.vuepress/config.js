b0efd30d1664db9af3b2841c187986d5104b5dfb
Bruce Yang <Bruce.Yang@kla-tencor.com>
2020-11-25 12:37:42 +0800

    Try to add sitemap.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -98,7 +98,11 @@ module.exports = {
     //   color: '#11a8cd', // 爱心颜色，默认随机色
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
-
+    ['sitemap', {
+      hostname: "https://dbdgs.cn",
+      // 排除无实际内容的页面
+      exclude: ["/404.html"]
+    }],
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
       thirdparty: [ // 可选，默认 []
         {
